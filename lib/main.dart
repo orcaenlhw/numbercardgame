4c66eb73cdde5cfb0815641ccaef5788ef37bd92
edn <45616806+theednorc@users.noreply.github.com>
2022-03-04 03:35:20 +0630

    upd

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:numbercardgame/components/image_btn.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,21 +12,79 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Select the Number',
+      title: 'Choose the Number',
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+      ),
+      home: SelectNumber(),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+class SelectNumber extends StatefulWidget {
+  SelectNumber({Key? key}) : super(key: key);
 
   @override
-  _HomePageState createState() => _HomePageState();
+  State<SelectNumber> createState() => _SelectNumberState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _SelectNumberState extends State<SelectNumber> {
+  List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  int correctAnswer = 0;
+  int score = 0;
+  SharedPreferences? prefs;
+
+  @override
+  void initState() {
+    super.initState();
+    loadScore();
+  }
+
+  saveScore(int newScore) async {
+    // obtain shared preferences
+    prefs = await SharedPreferences.getInstance();
+    if (prefs != null) {
+      await prefs?.setInt("score", newScore);
+    }
+  }
+
+  loadScore() async {
+    prefs = await SharedPreferences.getInstance();
+    if (prefs != null) {
+      var myscore = prefs?.getInt("score");
+      if (myscore != null) {
+        setState(() {
+          score = myscore;
+        });
+      }
+    }
+  }
+
+  Widget imageList(List<int> images) => Column(
+      children: images
+          .map((val) => ImageButton(
+              correctAnswer: correctAnswer,
+              val: val,
+              onTap: (ans) {
+                setState(() {
+                  score += ans;
+                  saveScore(score);
+                });
+              }))
+          .toList());
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    numbers.shuffle();
+    List<int> images = numbers.sublist(0, 3);
+    correctAnswer = images[0];
+    images.shuffle();
+
+    return Scaffold(
+      appBar: AppBar(title: const Text("Select Number")),
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+      ),
+    )
   }
 }
